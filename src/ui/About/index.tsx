71f3115e81f333c103e4db91cdd787755819b279
vietluu <74524082+vietluu@users.noreply.github.com>
2023-09-10 17:00:22 +0700

    fix/about ui

--- src/ui/About/index.tsx
+++ src/ui/About/index.tsx
@@ -5,10 +5,10 @@ import React from 'react';
 
 function index() {
   return (
-    <div className="fluid_container py-[50px] !bg-transparent">
+    <div className="fluid_container py-[10px] !bg-transparent">
       <div className="flex flex-wrap gap-7">
-        <div className="px-3 flex-grow-[8] basis-[500px] flex-shrink-0 mt-0">
-          <div className="p-6 bg-white">
+        <div className="px-3 sm:px-1 flex-grow-[8] basis-[300px] flex-shrink-0 mt-0">
+          <div className="p-6 md:p-2 bg-white">
             <h2 className="text-2xl text-sky-500 font-bold pb-3">Giới thiệu</h2>
             <p className="py-3">
               Trang giới thiệu giúp khách hàng hiểu rõ hơn về cửa hàng của bạn.
@@ -17,7 +17,7 @@ function index() {
               khi mua hàng trên website của bạn.
             </p>
             <p className="py-2">Một vài gợi ý cho nội dung trang Giới thiệu:</p>
-            <ul className="list-disc px-12">
+            <ul className="list-disc pl-12 sm:pl-8">
               <li>Bạn là ai</li>
               <li>Giá trị kinh doanh của bạn là gì</li>
               <li>Địa chỉ cửa hàng</li>
